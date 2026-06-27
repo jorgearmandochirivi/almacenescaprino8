f466b4fc6358ea40c63d1c709bece0e9b3837ef7
Jorge Chirivi <jorgechirivi@gmail.com>
2026-06-27 10:23:30 -0500

    Cierre de sesion

--- cerrarsesion.php
+++ cerrarsesion.php
@@ -1,7 +1,23 @@
 <?php
 include("admin/config.inc.php");
 
-$sql_cerrar="DELETE FROM Sesion WHERE IDUsuario in (13,109)";
+$Usuario=$_GET['Usuario'];
+switch ($Usuario) {
+    case "carolina":
+        $IDCerrar="13,109";
+        break;
+    case "arturo":
+        $IDCerrar="338";
+        break;
+    case "juliana":
+            $IDCerrar="190";
+        break;        
+    default:
+        $IDCerrar="13,109";
+        break;
+}
+$sql_cerrar="DELETE FROM Sesion WHERE IDUsuario in ($IDCerrar)";
 db_query($sql_cerrar);
+echo "Se cerró la sesión del usuario $Usuario";
 exit;
 ?>
